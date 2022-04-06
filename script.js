adea53eba6747763dcc87a40195f098203dd177a
Blaj3n <ulrichbence15@freemail.hu>
2022-04-06 10:51:35 +0200

    gombok kész + számok kiiratása a konzolra

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 window.addEventListener("load", init);
-function ID(elem){
-    return document.getElementById(elem);
+function ID(elem) {
+  return document.getElementById(elem);
 }
-function $(elem){
-    return document.querySelector(elem);
+function $(elem) {
+  return document.querySelector(elem);
 }
 
-function init(){
-    var txt = "";
-    for (let index = 0; index < 10; index++) {
-        txt += "<button>";
-        txt += [index];
-        txt += "</button>";
-    }
-    $(".szamok").innerHTML = txt;
+function init() {
+  var txt = "";
+  for (let index = 0; index < 10; index++) {
+    txt += "<button>";
+    txt += [index];
+    txt += "</button>";
+  }
+  $(".szamok").innerHTML = txt;
 
-    document.querySelector("button").addEventListener("click", szamBeutes);
-
-    function szamBeutes(){
-        document.querySelectorAll("button").innerHTML = "1";
-    }
+  for (let index = 0; index < 10; index++) {
+    document.querySelectorAll("button")[index].addEventListener("click", szamBeutes);
+  } 
 }
+function szamBeutes() {
+    console.log(event.target.innerHTML);
+  }
